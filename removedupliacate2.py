d25c4c817cc52fd9ef47b94067fd2e003562fd01
eddcode123 <edwinmutugi29@gmail.com>
2025-02-17 13:42:48 +0300

    Solution for removing duplicates from a list such that an element can only appeare atleast twice in order

---
+++ removedupliacate2.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """ Remove some duplicate in-place such that
+        each unique element only appears twice at most
+
+        Args:
+            nums (List[int]): list of integers
+
+        Returns:
+            int: the first n unique elements that appear atleast twice
+
+        Example:
+            >>> solution = Solution()
+            >>> result = solution.removeDuplicates([1,1,1,2,2,3])
+            >>> print(result)
+            5 [1,1,2,2,3,1]
+        """
+        insert_pos = 1
+        count = 1
+        max_occurrence = 2
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                count += 1
+            else:
+                count = 1
+        
+            if count <= max_occurrence:
+                if insert_pos != i:
+                    nums[insert_pos] = nums[i]
+                insert_pos += 1
+        
+        return insert_pos
+
+    
+# testcase
+solution = Solution()
+
+print(solution.removeDuplicates([0,0,1,1,1,1,2,3,3]))
+
+            
